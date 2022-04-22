79610cc1dbb3174d559288ac928d20cee0b1a1f8
Sanjeev Chitlapalli <sanjeevachitlapalli@gmail.com>
2022-04-22 17:44:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+function App() {
+  
+  return (
+    <div className="App">
+      <input value="" placeholder=''/>
+    </div>
+  );
+}
+
+export default App;
