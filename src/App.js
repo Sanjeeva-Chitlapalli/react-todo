333e928f4f89d775e9e593bb5237d664f7aadaca
Sanjeev Chitlapalli <sanjeevachitlapalli@gmail.com>
2022-04-22 21:04:18 +0530

    todo page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import TodoList from './TodoList';
 function App() {
   
   return (
     <div className="App">
-      <input value="" placeholder=''/>
+      <TodoList/>
     </div>
   );
 }
